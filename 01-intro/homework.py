36b68d74a06c95d1d6d310aa76ca961d7deca060
a920604a <a920604a@gmail.com>
2025-05-12 09:54:39 +0800

    Fix comments and update print statements for clarity in homework.py

--- 01-intro/homework.py
+++ 01-intro/homework.py
@@ -10,7 +10,7 @@ df = pd.read_parquet(
     "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
 )
 print("Q1. Downloading the data")
-print(f"Number of columns: {df.shape[1]}")
+print(f"Number of columns: {df.shape[1]}")  # 19
 
 
 # Q2. Computing duration
@@ -18,14 +18,14 @@ df["duration"] = (
     df.tpep_dropoff_datetime - df.tpep_pickup_datetime
 ).dt.total_seconds() / 60
 print("\nQ2. Computing duration")
-print(f"Standard deviation of duration: {df['duration'].std():.2f}")
+print(f"Standard deviation of duration: {df['duration'].std():.2f}")  # 42.59
 
 
 # Q3. Dropping outliers
 df_filtered = df[(df["duration"] >= 1) & (df["duration"] <= 60)]
 fraction_remaining = df_filtered.shape[0] / df.shape[0]
 print("\nQ3. Dropping outliers")
-print(f"Fraction of records remaining: {fraction_remaining:.2%}")  # 98%
+print(f"Fraction of records remaining: {fraction_remaining:.2%}")  # 98.12%
 
 
 # Q4. One-hot encoding
@@ -47,7 +47,7 @@ model.fit(X_train, y_train)
 y_pred_train = model.predict(X_train)
 rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
 print("\nQ5. Training a model")
-print(f"RMSE on train: {rmse_train:.2f}")  # 7.64
+print(f"RMSE on train: {rmse_train:.2f}")  # 7.65
 
 
 # Q6. Evaluating the model
@@ -69,4 +69,4 @@ y_val = df_val_filtered["duration"].values
 y_pred_val = model.predict(X_val)
 rmse_val = np.sqrt(mean_squared_error(y_val, y_pred_val))
 print("\nQ6. Evaluating the model")
-print(f"RMSE on validation: {rmse_val:.2f}")  # 11.81
+print(f"RMSE on validation: {rmse_val:.2f}")  # 7.81
